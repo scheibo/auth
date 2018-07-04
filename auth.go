31cd89ddfc538fe3f93857c1972ce4deae607361
Kirk Scheibelhut <kjs@scheibo.com>
2018-07-04 07:41:25 -0700

    Finish auth.go, needs to be tested

--- auth.go
+++ auth.go
@@ -1,133 +1,136 @@
 package auth
 
 import (
+	"crypto/sha512"
+	"html/template"
 	"net/http"
+	"time"
 
-	"golang.org/x/net/xsrftoken"
+	"github.com/gorilla/securecookie"
+
 	"golang.org/x/crypto/bcrypt"
+	"golang.org/x/net/xsrftoken"
 )
 
-// TODO must use SSL!!!!
-
-const DefaultLoginPath = "/login"
-const DefaultLogoutPath = "/logout"
-const DefaultSessionPath = "/login"
-const DefaultRedirectPath = "/"
+const LoginPath = "/login"
+const LogoutPath = "/logout"
+const RedirectPath = "/"
 
 type Client struct {
-	hash string
+	hash []byte
 
-	xsrfKey string
-	hashKey string
-	blockKey string
+	session *Session
+	cookie  securecookie.SecureCookie
 
-	loginPath string
-	logoutPath string
-	sessionPath string
-	redirectPath string
-
-	// TODO
-	// includes session store? - needs stte to know whether loged in or not?
-
-	// options: use ENCRYPTED cookie sotre
-	// allow header for Digest
-
-
-	// OPTION: 1) basic hashed password with xsrf
-	//         2) "Authorization: Hash <hash>" header?
-	//         3) Allow certificates (or ONLY allow certificates = need to auth only certain functions)
-
-
-	// TODO xsrf token timeout!
+	xsrfKey  string
+	hashKey  []byte
+	blockKey []byte
 }
 
-// key = 32 bits
-func New(options ...func(*Client)) (*Client, error) {
-	c := &Client{
-		xsrfKey: generateKey(),
-		hashfKey: generateKey(),
-		blockKey: generateKey(),
+type Session struct {
+	id      string
+	expires time.Time
+}
 
-		loginPath: DefaultLoginPath,
-		logoutPath: DefaultLogoutPath,
-		sessionPath: DefaultSessionPath,
-		redirectPath: DefaultRedirectPath,
+func Hash(password string) (string, error) {
+	sha := sha512.Sum512([]byte(password))
+	bytes, err := bcrypt.GenerateFromPassword(sha, bcrypt.DefaultCost)
+	return string(bytes), err
+}
+
+func New(hash string) *Client {
+	return &Client{
+		hash:     []byte(hash),
+		xsrfKey:  string(generateKey()),
+		hashKey:  generateKey(),
+		blockKey: generateKey(),
 	}
-	for _, option := range options {
-		err := option(c)
-		if err != nil {
-			return nil, err
+}
+
+func (c *Client) LoginPage(path ...string) *http.Handler {
+	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		loginPath := LoginPath
+		if len(path) > 0 && path[0] != "" {
+			loginPath = path[0]
+		}
+
+		t := template.Must(template.ParseFiles("login.html"))
+		t.Execute(w, struct {
+			Path  string
+			Token string
+		}{
+			loginPath,
+			c.XSRF(loginPath),
+		})
+	})
+}
+
+func (c *Client) Login(paths ...string) *http.Handler {
+	loginPath, redirectPath := LoginPath, RedirectPath
+	if len(path) == 1 && path[0] != "" {
+		loginPath = path[0]
+	}
+	if len(path) > 1 {
+		if path[0] != "" {
+			loginPath = path[0]
+		}
+		if path[1] != "" {
+			redirectPath = path[1]
 		}
 	}
 
-	return c, nil
+	return c.CheckXRSF(&http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !isHTTPS(req) {
+			return errors.New("login request must be over HTTPS")
+		}
+		if r.Method != "POST" {
+			return errors.New("login request must use POST")
+		}
+
+		var password, token = r.PostFormValue("password")
+		sha := sha512.Sum512([]byte(password))
+		err := bcrypt.CompareHashAndPassword(hash, sha)
+		if err != nil {
+			httpError(w, 401)
+			return
+		}
+
+		c.session = &Session{
+			id:      string(generateKey()),
+			expires: time.Now().AddDate(0, 0, 30),
+		}
+		//w.Header().Set("Authorization", fmt.Sprintf("Token %s", c.session.id))
+		cookie, err := c.newCookie()
+		if err != nil {
+			httpError(w, 500)
+			return
+		}
+		http.SetCookie(w, cookie)
+
+		http.Redirect(w, r, redirectPath, 302)
+	}), loginPath)
 }
 
-func LoginPath(path string) func(*Client) error {
-	return func(*Client) error {
-		c.loginPath = path
-	}
-}
+func (c *Client) Logout(paths ...string) *http.Handler {
+	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		redirectPath := RedirectPath
+		if len(path) > 0 && path[0] != "" {
+			redirectPath = path[0]
+		}
 
-func LogoutPath(path string) func(*Client) error {
-	return func(*Client) error {
-		c.logoutPath = path
-	}
-}
+		c.session = nil
+		//w.Header().Del("Authorization")
+		http.SetCookie(w, &http.Cookie{
+			Name:     "Authorization",
+			Value:    "",
+			Secure:   true,
+			HttpOnly: true,
+			Path:     "/",
+			Expires:  time.Unix(0, 0),
+		})
 
-func SessionPath(path string) func(*Client) error {
-	return func(*Client) error {
-		c.sessionPath = path
-	}
-}
-
-func RedirectPath(path string) func(*Client) error {
-	return func(*Client) error {
-		c.redirectPath = path
-	}
-}
-
-func (c *Client) LoginPage() error {
-// returns form to be rendered at LoginPath
-// TODO provide option for just getting secret for xsrf and allowing site to hsot own login
-
-
-// 1) generate XSRF token, include in page
-// 2) include path to POST to
-// 3) render page
-}
-
-func (c *Client) Login(w http.ResponseWriter, r *http.Request) {
-	if !isHTTPS(req) {
-		return errors.New("login request must be over HTTPS")
-	}
-	if r.Method != "POST" {
-		return errors.New("login request must use POST")
-	}
-
-	r.ParseForm()
-	var password, token = r.Form["password"], r.Form["token"]
-	if !xsrftoken.Valid(token, c.key, "", "") {
-		httpError(w, 401)
-		return
-	}
-	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
-	if err != nil {
-		httpError(w, 401)
-		return
-	}
-// TODO set cookie!
-	http.Redirect(w, r, c.RedirectPath, 302)
-}
-
-func (c *Client) Logout(w http.ResponseWriter, r *http.Request) {
-	// TODO clear cookie
-	http.Redirect(w, r, c.RedirectPath, 302)
-}
-
-func (c *Client) Verify(handler *http.Handler) *http.Handler {
-	// TODO optionall around a path?
-	return c.EnsureAuth(c.protectXSRF(handler)
+		http.Redirect(w, r, redirectPath, 302)
+	})
 }
 
 func (c *Client) XSRF(path ...string) string {
@@ -135,49 +138,73 @@ func (c *Client) XSRF(path ...string) string {
 	if len(path) > 0 {
 		p = path[0]
 	}
-	return xsrftoken.Generate(c.key, "", p)
+	return xsrftoken.Generate(c.xsrfKey, "", p)
 }
 
-func (c *Client) CheckXSRF(w http.ResponseWriter, r *http.Request, path ...string) {
-	p := ""
-	if len(path) > 0 {
-		p = path[0]
-	}
+func (c *Client) CheckXSRF(handler *http.Handler, path ...string) *http.Handler {
+	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p := ""
+		if len(path) > 0 {
+			p = path[0]
+		}
 
-	r.ParseForm()
-	token := r.Form["token"]
-	if !xsrftoken.Valid(r.Form["token"], c.key, "", p) {
-		httpError(w, 403)
-		return
-	}
-}
-
-func (c *Client) IsAuth(req *http.Request) bool {
-	// TODO check cookie
+		if !xsrftoken.Valid(r.PostFormValue("token"), c.xsrfKey, "", p) {
+			httpError(w, 401)
+			return
+		}
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func (c *Client) EnsureAuth(handler *http.Handler) *http.Handler {
 	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !c.IsAuth() {
-			httpError(w, 403)
+			httpError(w, 401)
 			return
 		}
 		handler.ServeHTTP(w, r)
+	})
+}
+
+func (c *Client) IsAuth(req *http.Request) bool {
+	if c.session == nil {
+		return false
 	}
+	if c.session.expires.Before(time.Now()) {
+		return false
+	}
+	//if req.Header.Get("Authorization") == fmt.Sprintf("Token %s", c.session.id) {
+	//return true
+	//}
+	if cookie, err := r.Cookie(cookieName); err == nil {
+		value := make(map[string]string)
+		if err = c.cookie.Decode(cookieName, cookie.Value, &value); err == nil {
+			return value["Authorization"] == c.session.id
+		}
+	}
+	return false
 }
 
-func (c *Client) Hash(password string) string, error {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	return string(bytes), err
+func (c *Client) newCookie() (*http.Cookie, error) {
+	s := securecookie.New(c.hashKey, c.blockKey)
+	err := s.Encode("Authorization", c.session.id)
+	if err != nil {
+		return nil, err
+	}
+
+	c.cookie = s
+	return &http.Cookie{
+		Name:     "Authorization",
+		Value:    encoded,
+		Secure:   true,
+		HttpOnly: true,
+		Path:     "/",
+		Expires:  c.session.expires,
+	}, nil
 }
 
-func (c *Client) generateKey() string {
+func generateKey() string {
 	return securecookie.GenerateRandomKey(32)
-}
-
-func (c *Client) newCookie() {
-	var hashKey = []byte(c.hashKey)
-	var blockKey = []byte(securecookie.GenerateRandomKey(32))
 }
 
 func isHTTPS(req *http.Request) bool {
@@ -187,6 +214,3 @@ func isHTTPS(req *http.Request) bool {
 func httpError(w http.ResponseWriter, code int) {
 	http.Error(w, http.StatusText(code), code)
 }
-
-
-
