08c7ce131fd4390a2cb8173cb4ea6f1d2924c03a
Kirk Scheibelhut <kjs@scheibo.com>
2018-07-05 17:57:37 -0700

    Builds

--- a1.go
+++ a1.go
@@ -5,12 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"path/filepath"
 	"time"
 
-	"github.com/didip/tollbooth/limiter"
+	"github.com/didip/tollbooth"
 	"github.com/gorilla/securecookie"
 	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/html"
 
 	"golang.org/x/crypto/bcrypt"
 	"golang.org/x/net/xsrftoken"
@@ -20,11 +23,13 @@ const LoginPath = "/login"
 const LogoutPath = "/logout"
 const RedirectPath = "/"
 
+const CookieName = "Authorization"
+
 type Client struct {
 	hash []byte
 
 	session *Session
-	cookie  securecookie.SecureCookie
+	cookie  *securecookie.SecureCookie
 
 	xsrfKey  string
 	hashKey  []byte
@@ -38,7 +43,7 @@ type Session struct {
 
 func Hash(password string) (string, error) {
 	sha := sha512.Sum512([]byte(password))
-	bytes, err := bcrypt.GenerateFromPassword(sha, bcrypt.DefaultCost)
+	bytes, err := bcrypt.GenerateFromPassword(sha[:64], bcrypt.DefaultCost)
 	return string(bytes), err
 }
 
@@ -51,12 +56,12 @@ func New(hash string) *Client {
 	}
 }
 
-func (c *Client) LoginPage(path ...string) *http.Handler {
-	return CustomLoginPage("https://raw.githubusercontent.com/scheibo/auth/master/favicon.png", "Login")
+func (c *Client) LoginPage(path ...string) http.Handler {
+	return c.CustomLoginPage("https://raw.githubusercontent.com/scheibo/auth/master/favicon.png", "Login")
 }
 
-func (c *Client) CustomLoginPage(favicon, title string, path ...string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (c *Client) CustomLoginPage(favicon, title string, path ...string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !isHTTPS(r) {
 			httpError(w, 500, errors.New("login page must be served over HTTPS"))
 		}
@@ -67,7 +72,7 @@ func (c *Client) CustomLoginPage(favicon, title string, path ...string) *http.Ha
 		}
 
 		t := template.Must(compileTemplates("login.html"))
-		t.Execute(w, struct {
+		_ = t.Execute(w, struct {
 			Favicon string
 			Title   string
 			Path    string
@@ -78,22 +83,22 @@ func (c *Client) CustomLoginPage(favicon, title string, path ...string) *http.Ha
 	})
 }
 
-func RateLimit(handler *httpHandler, qps float64) *http.Handler {
-	return &tollbooth.LimitFuncHandler(tollbooth.NewLimiter(qps, nil), handler)
+func RateLimit(handler http.Handler, qps float64) http.Handler {
+	return tollbooth.LimitFuncHandler(tollbooth.NewLimiter(qps, nil), handler.ServeHTTP)
 }
 
-func (c *Client) Login(paths ...string) *http.Handler {
+func (c *Client) Login(paths ...string) http.Handler {
 	loginPath, redirectPath := LoginPath, RedirectPath
-	if len(path) >= 1 {
-		if path[0] != "" {
-			loginPath = path[0]
+	if len(paths) >= 1 {
+		if paths[0] != "" {
+			loginPath = paths[0]
 		}
-		if len(path) > 1 && path[1] != "" {
-			redirectPath = path[1]
+		if len(paths) > 1 && paths[1] != "" {
+			redirectPath = paths[1]
 		}
 	}
 
-	return RateLimit(c.CheckXRSF(&http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	return RateLimit(c.CheckXSRF(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if !isHTTPS(r) {
 			httpError(w, 500, errors.New("login request must be over HTTPS"))
 		}
@@ -101,9 +106,9 @@ func (c *Client) Login(paths ...string) *http.Handler {
 			httpError(w, 500, errors.New("login request must use POST"))
 		}
 
-		var password, token = r.PostFormValue("password")
+		password := r.PostFormValue("password")
 		sha := sha512.Sum512([]byte(password))
-		err := bcrypt.CompareHashAndPassword(hash, sha)
+		err := bcrypt.CompareHashAndPassword(c.hash, sha[:64])
 		if err != nil {
 			httpError(w, 401, err)
 			return
@@ -124,8 +129,8 @@ func (c *Client) Login(paths ...string) *http.Handler {
 	}), loginPath), 1)
 }
 
-func (c *Client) Logout(paths ...string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (c *Client) Logout(path ...string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		redirectPath := RedirectPath
 		if len(path) > 0 && path[0] != "" {
 			redirectPath = path[0]
@@ -153,24 +158,24 @@ func (c *Client) XSRF(path ...string) string {
 	return xsrftoken.Generate(c.xsrfKey, "", p)
 }
 
-func (c *Client) CheckXSRF(handler *http.Handler, path ...string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func (c *Client) CheckXSRF(handler http.Handler, path ...string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		p := ""
 		if len(path) > 0 {
 			p = path[0]
 		}
 
 		if !xsrftoken.Valid(r.PostFormValue("token"), c.xsrfKey, "", p) {
-			httpError(w, 401, errors.New("invalid XSRF")
+			httpError(w, 401, errors.New("invalid XSRF"))
 			return
 		}
 		handler.ServeHTTP(w, r)
 	})
 }
 
-func (c *Client) EnsureAuth(handler *http.Handler) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !c.IsAuth() {
+func (c *Client) EnsureAuth(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !c.IsAuth(r) {
 			httpError(w, 401)
 			return
 		}
@@ -186,13 +191,13 @@ func (c *Client) IsAuth(r *http.Request) bool {
 		return false
 	}
 	// Useful for debugging with curl - this is *not* a valid digest auth header.
-	if r.Header.Get("Authorization") == fmt.Sprintf("Hash %s", c.hash) {
-	  return true
+	if r.Header.Get(CookieName) == fmt.Sprintf("Hash %s", c.hash) {
+		return true
 	}
-	if cookie, err := r.Cookie(cookieName); err == nil {
+	if cookie, err := r.Cookie(CookieName); err == nil {
 		value := make(map[string]string)
-		if err = c.cookie.Decode(cookieName, cookie.Value, &value); err == nil {
-			return value["Authorization"] == c.session.id
+		if err = c.cookie.Decode(CookieName, cookie.Value, &value); err == nil {
+			return value[CookieName] == c.session.id
 		}
 	}
 	return false
@@ -200,14 +205,14 @@ func (c *Client) IsAuth(r *http.Request) bool {
 
 func (c *Client) newCookie() (*http.Cookie, error) {
 	s := securecookie.New(c.hashKey, c.blockKey)
-	err := s.Encode("Authorization", c.session.id)
+	encoded, err := s.Encode(CookieName, c.session.id)
 	if err != nil {
 		return nil, err
 	}
 
 	c.cookie = s
 	return &http.Cookie{
-		Name:     "Authorization",
+		Name:     CookieName,
 		Value:    encoded,
 		Secure:   true,
 		HttpOnly: true,
@@ -216,7 +221,7 @@ func (c *Client) newCookie() (*http.Cookie, error) {
 	}, nil
 }
 
-func generateKey() string {
+func generateKey() []byte {
 	return securecookie.GenerateRandomKey(32)
 }
 
@@ -224,7 +229,7 @@ func isHTTPS(r *http.Request) bool {
 	return r.TLS != nil
 }
 
-func httpError(w http.ResponseWriter, code int, err...error) {
+func httpError(w http.ResponseWriter, code int, err ...error) {
 	msg := http.StatusText(code)
 	if len(err) > 0 {
 		msg = fmt.Sprintf("%s: %s", msg, err[0].Error())
@@ -254,7 +259,10 @@ func compileTemplates(filenames ...string) (*template.Template, error) {
 		if err != nil {
 			return nil, err
 		}
-		tmpl.Parse(string(mb))
+		_, err = tmpl.Parse(string(mb))
+		if err != nil {
+			return nil, err
+		}
 	}
 	return tmpl, nil
 }
